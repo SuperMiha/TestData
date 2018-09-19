bfda9aa2771856ef3be9b03ed8ff5ff97e0eb19b
SuperMiha <43410989+SuperMiha@users.noreply.github.com>
2018-09-19 17:44:18 +0300

    add

---
+++ monitor/monitor.js
@@ -0,0 +1,49 @@
+window.CSApplication = window.CSApplication || {};
+
+/**
+ * Модуль отображения тестовой информации
+ */
+class MonitorModule {
+
+    /**
+     * Инициализация свойств
+     */
+    properties() {
+    }
+
+    /**
+     * Инициализация модуля
+     */
+    constructor() {
+        this.properties();
+    }
+
+    /**
+     * Отображение схемы подстанции
+     */
+    showSubstationScheme() {
+        try {
+            // Показываем загрузочный спиннер (опционально, в качестве примера)
+            CSAPI.v1.showSpinner(true).then(() => {
+                // Получаем атрибуты объекта
+                const objectData = CSAPI.v1.getDataObjects("search").data[0].attributes;
+                // Получаем атрибут "Дисп.наим." (Пример: {label:"Дисп.наим.", value:"ТП-81"})
+                const attrObject = objectData.find(attr => attr.label === "Дисп.наим.");
+                //---------------------------------------------
+                // ЗДЕСЬ ВАША ЛОГИКА
+                //---------------------------------------------
+                // Скрываем загрузочный спиннер
+                CSAPI.v1.showSpinner(false);
+            });
+
+        } catch (err) {
+            // Сообщение об ошибке
+            console.log(err);
+            // Скрываем загрузочный спиннер
+            CSAPI.v1.showSpinner(false);
+        }
+    }
+}
+
+// Ссылка на глобальный доступ к классу модулю
+CSApplication.monitorModule = new MonitorModule();
